df13cb50087ade2c065cbad2c854fae8642f6814
jsw <runner6@hanmail.net>
2024-01-17 20:50:54 +0900

    kakao map 추가#2

--- js/base.js
+++ js/base.js
@@ -75,7 +75,7 @@ $(function() {
     }
     diffDay();
 
-    const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    const mapContainer = document.getElementById('navi_map'), // 지도를 표시할 div
         mapOption = {
             center: new kakao.maps.LatLng(37.56020032129378, 126.83930631305121), // 지도의 중심좌표
             level: 3 // 지도의 확대 레벨
