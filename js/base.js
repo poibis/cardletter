550359a5f2440066f27663a483ad0e9253470fd8
jsw <runner6@hanmail.net>
2024-01-17 20:44:02 +0900

    kakao map 추가

--- js/base.js
+++ js/base.js
@@ -75,6 +75,36 @@ $(function() {
     }
     diffDay();
 
+    const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+        mapOption = {
+            center: new kakao.maps.LatLng(37.56020032129378, 126.83930631305121), // 지도의 중심좌표
+            level: 3 // 지도의 확대 레벨
+        };
+
+    const map = new kakao.maps.Map(mapContainer, mapOption);
+
+    // 마커가 표시될 위치입니다
+    const markerPosition  = new kakao.maps.LatLng(37.56020032129378, 126.83930631305121);
+
+    // 마커를 생성합니다
+    const marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    var iwContent = '<div style="padding:5px; text-align:center;">더베뉴지 서울<br>강서구 강서로 388</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+// 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        position : iwPosition,
+        content : iwContent
+    });
+
+// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+    infowindow.open(map, marker);
 });
 
 // windowLoad
