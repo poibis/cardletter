b63126b7c27e7825f8225869b92271d6039a67db
jsw <runner6@hanmail.net>
2024-02-01 20:56:00 +0900

    release ver2.26dev -캐시 이미지 2번 로드 점검 -마무리

--- js/scrollAction.js
+++ js/scrollAction.js
@@ -47,6 +47,51 @@ new ScrollMagic.Scene({
 const topPin    = $('#top_pin').offset().top;
 const middlePin    = $('#middle_pin').offset().top;
 
+
+
+// 크래프트 이미지 파일 경로를 저장할 배열을 생성합니다.
+const craftImgSqc = [];
+
+// 이미지 파일 경로를 배열에 저장합니다.
+for (let i = 1; i < 141; i++) {
+    craftImgSqc.push(`https://dn730rlq8l0gj.cloudfront.net/img/sequence/${i}.jpg`);
+}
+
+// 이미지를 사전에 로드하기 위한 함수를 정의합니다.
+function preloading(craftpreImgs) {
+    let craftimgTotal = craftpreImgs.length;
+
+    // 각 이미지 경로에 대해 Image 객체를 생성하여 이미지를 로드합니다.
+    for (let i = 0; i < craftimgTotal; i++) {
+        let craftimg = new Image();
+        craftimg.src = craftpreImgs[i];
+    }
+}
+
+// 배열에 담긴 크래프트 이미지들을 미리 로드합니다.
+preloading(craftImgSqc);
+
+// 현재 이미지 인덱스를 저장할 객체를 생성합니다.
+const craftimg = { craftcrntImg: 0 },
+    // 'craft' 클래스를 가진 HTML 엘리먼트를 선택하여 이미지를 표시합니다.
+    craftimgTag = document.querySelector('.craft');
+
+let imgTween = gsap.timeline();
+
+// TweenMax를 사용하여 이미지 간 부드러운 전환을 담당하는 애니메이션을 생성합니다.
+let packageObj = imgTween.to(craftimg, 0.1, {
+    craftcrntImg: craftImgSqc.length - 1,
+    roundProps: 'craftcrntImg',
+    immediateRender: true,
+    // Tween 업데이트 시마다 실행되는 콜백 함수입니다.
+    onUpdate: function () {
+        // craftimgTag의 'src' 속성을 현재 이미지의 경로로 설정합니다.
+        $("#myimg").attr("src", craftImgSqc[craftimg.craftcrntImg]); // set the image source
+
+        // craftimgTag.setAttribute('src', craftImgSqc[craftimg.craftcrntImg]);
+    }
+});
+
 // define images
 let images = [];
 
@@ -55,46 +100,13 @@ for(let i=1; i<141; i++) {
     images.push('https://dn730rlq8l0gj.cloudfront.net/img/sequence/'+i+'.jpg');
 }
 
-/**
- * ? preload image cache 처리
- *
- **/
-let imgSrcs = [];
-//const IMG_PRE_URL = "https://www.jswjake.xyz/carletter/img/sequence";
-const IMG_PRE_URL = "https://dn730rlq8l0gj.cloudfront.net/img/sequence";
-for (let i = 1; i < 141; i++) {
-    imgSrcs.push(`${IMG_PRE_URL}/`+i+`.jpg`);
-}
-
-function preloadImage(src) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = src;
-        img.onload = resolve;
-        img.onerror = reject;
-    })
-}
-
-async function preloadImages() {
-    const startTime = new Date().getTime();
-    $(".standBy").css("display","none");
-    await
-        Promise.all(imgSrcs.map(preloadImage));
-    const endTime = new Date().getTime();
-    $(".standBy").fadeIn(2500);
-    $(".preLoad").css("display","none");
-    console.log(endTime - startTime);
-}
-preloadImages();
-
-
 // TweenMax can tween any property of any object. We use this object to cycle through the array
 let obj = {curImg: 0};
 
 // create tween
-let imgTween = gsap.timeline();
+/*let imgTween = gsap.timeline();
 
-const packageObj = imgTween.to(obj, 1,
+const packageObj = imgTween.to(obj, 0.1,
     {
         curImg: images.length - 1,	        // animate proper curImg to number of images
         roundProps: "curImg",				// only integers so it can be used as an array index
@@ -105,7 +117,7 @@ const packageObj = imgTween.to(obj, 1,
             $("#myimg").attr("src", imgSrcs[obj.curImg]); // set the image source
         }
     },
-);
+);*/
 
 // build scene
 new ScrollMagic.Scene({
